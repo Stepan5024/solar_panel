d49b8f650a317641a7b768746d6e3fb93efe7759
Степан Бокарев <bokarevstepan1@yandex.ru>
2024-04-08 16:44:50 +0300

    Тестирование по видео

--- main.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
-
-# Use the model
-results = model.train(data="config.yaml", epochs=100)  # train the model
-#model.val()  # оцените производительность модели на наборе проверки
